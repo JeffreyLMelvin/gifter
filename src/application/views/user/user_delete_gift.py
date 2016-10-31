02f5355037f9004b94a1ef76252742cb3aa3a512
Jeffrey Melvin <aetherdeity@gmail.com>
2016-10-30 23:16:41 -0500

    notify purchaser when purchased item deleted

--- src/application/views/user/user_delete_gift.py
+++ src/application/views/user/user_delete_gift.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from twilio.rest import TwilioRestClient
+
 from flask.views import View
 
 from flask import flash, redirect, url_for, request, session
@@ -7,8 +9,12 @@ from flask import flash, redirect, url_for, request, session
 from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
 
 from models import GiftModel
+from models import UserModel
 
 from decorators import registration_required
+
+from settings import TWILIO_SID
+from settings import TWILIO_TOKEN
 
 
 class DeleteGift(View):
@@ -19,6 +25,30 @@ class DeleteGift(View):
         if request.method == "POST":
             if gift.added_by.id() == session['user']['uid']:
                 try:
+                    if gift.purchaser:
+                        users = UserModel.query()
+                        purchaser = None
+                        owner = None
+                        adder = None
+                        for user in users:
+                            purchaser = user if user.key.id() == gift.purchaser.id() else purchaser
+                            owner = user if user.key.id() == gift.owner.id() else owner
+                            adder = user if user.key.id() == gift.added_by.id() else adder
+
+                        client = TwilioRestClient(TWILIO_SID, TWILIO_TOKEN)
+                        message = client.messages.create(
+                            body="%s %s removed %s from %s %s's list, "
+                                 "which you purchased" % (
+                                    adder.user_first_name,
+                                    adder.user_last_name,
+                                    gift.summary,
+                                    owner.user_first_name,
+                                    owner.user_last_name
+                                ),
+                            to=purchaser.user_phone,
+                            from_='+15153052239'
+                        )
+
                     gift.key.delete()
                     flash(u'Gift %s successfully deleted.' % gift_id, 'success')
                     return redirect(url_for('gift_list', user_id=gift.owner.id()))
